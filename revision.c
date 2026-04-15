8c58accaa3a9fef195ea67602215a1c1a8a1acca
anushkashukla5281-cpu <anushkashukla5281@gmail.com>
2026-04-15 21:27:42 +0530

    Add files via upload

---
+++ revision.c
@@ -0,0 +1,115 @@
+# include <stdio.h>
+int main() {
+  /* printf("Hello, World");
+    return 0;*/
+//area of circle
+/*float radius, area , PI=3.14 ;
+printf("please enter the radius of the circle:- ");
+scanf("%f", &radius);
+area = PI *radius *radius ;
+printf("the area of the circle is : %f \n", area);*/
+//area of the square 
+/*float length , breath, area ;
+printf (" please enter the lenghth square ");
+scanf ("%f" , &length );
+printf("please enter the breath of the square ");
+scanf("%f" , &breath);
+area= length * breath ;
+printf(" the area of the square is : %f \n" , area );*/
+
+
+//program to calculate the sum of two integers input by user 
+
+/*float a , b , sum  ;
+printf(" enter the first no. - ");
+scanf("%f" , &a);
+printf("enter the second no. - ");
+scanf("%f", &b);
+sum = a+b ;
+printf("the sum of both the numbers is :- %f/n" , sum);*/
+
+// program of simple interest ; 
+ /*float P , R , T , SI ;
+ printf(" enter the principal amt -  ");
+ scanf("%f", &P);
+ printf("enter the rate of interest - ");
+ scanf("%f" , &R);
+ printf(" enter the time - ");
+ scanf("%f" , &T );
+ SI = P*R*T / 100 ;
+ printf (" simple interest is-%f\n" , SI);*/
+
+
+
+
+ // SP and CP calculating the profit and loss 
+ /*float cp , sp , profit_loss ;
+ printf(" enter cost price per dozen : ");
+ scanf("%f" , &cp );
+ printf("enter the selling price per dozen: ");
+ scanf("%f" , &sp );
+ //cost and selling price for 25 bananas 
+ float total_cp = (cp/12)*25 ;
+ float total_sp = (sp/12)*25  ;
+profit_loss = total_sp - total_cp ;
+if (profit_loss > 0 )
+    printf("profit = %f", profit_loss);
+    else if (profit_loss < 0)
+    printf("loss = %f", profit_loss);
+    else printf("no profit no loss");*/
+
+//LOOPS REVISION 
+
+/*int i = 1 ;
+while ( i <= 5 ){
+printf("%d\n ", i);
+i++;
+}*/
+
+
+/*int i=2 ;
+while ( i <= 10 ) {
+    printf("%d" , i );
+    i += 2;
+}*/
+//odd no.
+/*int i = 1 ;
+while (i <= 9 ){
+    printf("%d" , i);
+    i += 2;
+
+}*/
+
+// print the table of 5 
+
+/*int i = 1 ; 
+while ( i <=10 ){
+    printf("5* %d = %d\n" , i , 5*i);
+    i++ ; 
+}*/
+
+
+//reverse numbers 5 to 1 
+/*int i = 5 ;
+while (i >= 1) {
+
+    printf("%d" , i );
+    i -- ;
+}*/
+
+// print the square of the numbers 1 to 5 
+/*int i=5 ;
+while ( i <=5) {
+    printf("%d" , i*i) ; 
+    i++ ;
+}*/
+
+
+
+
+
+
+
+
+
+}
