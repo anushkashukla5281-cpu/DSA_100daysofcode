3319f250638aa4a42cc775be6f938d80070242b7
anushkashukla5281-cpu <anushkashukla5281@gmail.com>
2026-08-16 23:24:11 +0530

    Add files via upload

---
+++ day05_second_largest.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int largest = arr[0];
+    int secondLargest = -1;
+
+    for (int i = 1; i < n; i++) {
+
+        if (arr[i] > largest) {
+            secondLargest = largest;
+            largest = arr[i];
+        }
+        else if (arr[i] > secondLargest && arr[i] != largest) {
+            secondLargest = arr[i];
+        }
+    }
+
+    cout << "Second largest = " << secondLargest;
+
+    return 0;
+}
